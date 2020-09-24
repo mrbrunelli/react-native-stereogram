1285216bf2a20e50f1f0d7dedacc6c5b7ed84949
Matheus R. Brunelli <matheus.brunelli@gmail.com>
2020-09-24 00:32:43 -0300

    feat: add ios transition

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
+import { createStackNavigator, CardStyleInterpolators } from '@react-navigation/stack'
 import Home from './pages/Home'
 import Detail from './pages/Detail'
 
@@ -9,7 +9,14 @@ const Stack = createStackNavigator()
 const Routes = (): React.ReactElement => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          gestureEnabled: true,
+          gestureDirection: 'horizontal',
+          cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS
+        }}
+      >
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Detail" component={Detail} />
       </Stack.Navigator>

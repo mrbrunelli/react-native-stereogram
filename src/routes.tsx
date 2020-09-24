751f440c4d0129bb23b885590cef4fe59a4cab73
Matheus R. Brunelli <matheus.brunelli@gmail.com>
2020-09-23 23:01:12 -0300

    feat: add home screen

---
+++ src/routes.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import Home from './pages/Home'
+
+const Stack = createStackNavigator()
+
+const Routes = (): React.ReactElement => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default Routes
